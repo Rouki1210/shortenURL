b699276913f38c97c4cda173689210652e0ddb78
Vinh Nguyen <vinh12102004@gmail.com>
2024-12-08 23:40:09 +0700

    .

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -3,28 +3,25 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
-// Add services to the container.
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddCors(options =>
+builder.Configuration.AddJsonFile(
+  "ocelot.json",
+  optional: false,
+  reloadOnChange: true
+  );
+
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(x =>
 {
-    options.AddPolicy("CorsPolicy", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
+    x.WithDictionaryHandle();
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseCors("CorsPolicy");
-app.UseHttpsRedirection();
-await app.UseOcelot();
-
+//app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseOcelot().Wait();
 
 
 app.Run();
-
