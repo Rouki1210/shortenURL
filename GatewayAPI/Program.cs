e63962f7619e667defc2e5a35a7ccb1716cbbf69
admin <vinh12102004@gmail.com>
2024-12-09 15:10:59 +0700

    d

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,9 +1,13 @@
-using Ocelot.Cache.CacheManager;
+using Microsoft.EntityFrameworkCore;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Service1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddDbContext<UrlContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Configuration.AddJsonFile(
   "ocelot.json",
@@ -11,17 +15,27 @@ builder.Configuration.AddJsonFile(
   reloadOnChange: true
   );
 
-builder.Services.AddOcelot(builder.Configuration).AddCacheManager(x =>
+builder.Services.AddOcelot(builder.Configuration);
+
+//(1) config CORS   => VERY IMPORTANT
+builder.Services.AddCors(options =>
 {
-    x.WithDictionaryHandle();
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 });
 
 var app = builder.Build();
+
+//(2) enable CORS    => VERY IMPORTANT
+app.UseCors("CorsPolicy");
 
 //app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.UseOcelot().Wait();
-
 
 app.Run();
